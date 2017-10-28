df9e55a9c23cb04ab62c356142dccfaa52b26933
Deepak Sharma <dpkshrma01@gmail.com>
2017-10-28 17:43:34 +0530

    Fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import mapboxgl from 'mapbox-gl';
 import Slider from 'rc-slider';
+import styled from 'styled-components';
 import { addHeatMap } from './helpers';
 import 'rc-slider/assets/index.css';
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
+
+const MapContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
 
 const style = {
   width: '100vw',
   height: '100vh'
 };
-
-mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
 
 const styles = {
   slider: {
@@ -17,50 +23,51 @@ const styles = {
     bottom: 0,
     width: '100%',
     minHeight: 64,
-    background: 'rgba(255,255,255,0.5)',
+    background: 'rgba(0,0,0,0.9)',
   }
 };
 
-const marks = {
-  0: '1999',
-  10: '2000',
-  20: '2001',
-  30: '2002',
-  40: '2003',
-  50: '2004',
-  60: '2005',
-  70: '2006',
-  80: '2007',
-  90: '2008',
+const years = {
+  0: 1999,
+  10: 2000,
+  20: 2001,
+  30: 2002,
+  40: 2003,
+  50: 2004,
+  60: 2005,
+  70: 2006,
+  80: 2007,
+  90: 2008,
   100: ''
 };
 
-const log = value => {
-  console.log(value);
-}
-
-const InputSlider = () => {
+const InputSlider = ({ marks, onChange }) => {
   return (
     <div style={styles.slider}>
-      <Slider min={-10} marks={marks} step={null} onChange={log} defaultValue={20} />
+      <Slider min={-10} marks={marks} step={null} defaultValue={20} onChange={onChange} />
     </div>
   );
-}
+};
 
 class App extends Component {
   componentDidMount() {
-    const map = new mapboxgl.Map({
+    this.mapboxMapRef = new mapboxgl.Map({
       container: this.map,
       style: 'mapbox://styles/mapbox/dark-v9',
       zoom: 2,
     });
-    addHeatMap(map);
+    addHeatMap(this.mapboxMapRef);
+  }
+  onYearChange = year => {
+    const filters = ['==', 'year', year];
+    this.mapboxMapRef.setFilter('earthquakes-heat', filters);
+    this.mapboxMapRef.setFilter('earthquakes-point', filters);
   }
   render() {
     return (
       <div>
-        <div style={style} ref={map => { this.map = map; }} />
-        <InputSlider />
+        <MapContainer style={style} innerRef={map => { this.map = map; }} />
+        <InputSlider onChange={this.onYearChange} marks={years} />
       </div>
     );
   }
